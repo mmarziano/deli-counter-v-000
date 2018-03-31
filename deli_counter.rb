3a326ab08a422518b1d42de8873c449a05128d59
Monica Marziano <mmarziano1@gmail.com>
2018-03-31 10:49:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    katz_deli
+    puts "The line is currently: #{katz_deli.index}. #{katz_deli} "
   end
     
 end
