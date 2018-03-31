022bd58f52d57c01b5335ba66f2006b2e872fc33
Monica Marziano <mmarziano1@gmail.com>
2018-03-31 10:54:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,10 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: #{katz_deli.index}. #{katz_deli} "
+    new_array = []
+    katz_deli.each_with_index do |name, index|
+      new_array << "#{index}. #{name}"
+    puts "The line is currently: #{new_array}."
   end
     
 end
